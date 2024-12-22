4c46be5a31e34d7ece7225401fdb54807bd996a2
MurathanTas <murathantas03@gmail.com>
2024-12-22 20:09:44 +0300

    ContactController eklendi

---
+++ BerberWeb.UI/Controllers/AdminContactController.cs
@@ -0,0 +1,34 @@
+﻿using BerberWeb.Business.Abstract;
+using BerberWeb.Entity.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BerberWeb.UI.Controllers
+{
+    public class AdminContactController : Controller
+    {
+        private readonly IGenericService<Contact> _contactService;
+
+        public AdminContactController(IGenericService<Contact> contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public IActionResult Index()
+        {
+            var about = _contactService.TGetByID(1);
+            return View(about);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _contactService.TUpdate(contact);
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+    }
+}
